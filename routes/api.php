4fbe0e7c141fba5153330b110907534e9f301a48
Omaralaj <alajatiomar57@gmail.com>
2024-11-24 19:06:59 +0300

    migrate session

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
@@ -12,6 +13,11 @@ Route::delete('patients/{id}', [PatientController::class, 'delete']);
 Route::put('patientss/{id}', [PatientController::class, 'update']);
 Route::post('login', [PatientController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [PatientController::class, 'logout']);
+/////////////////////DOCTOR////////////////////////////////
+Route::post('doctor/register', [DoctorController::class, 'register']);
+
+
+
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
